d148e717fd76910ed5d52c03ea6480e2352e5b9d
halimocakli <halimcanocakli@gmail.com>
2024-04-24 14:15:39 +0300

    (feat): comments have added

--- 02/02-Alias/main.tf
+++ 02/02-Alias/main.tf
@@ -1,3 +1,4 @@
+# Bu blok, gereken Terraform ve Azure sağlayıcısının gereken sürümünü belirtir
 terraform {
   required_version = "~> 1.1"
   required_providers {
@@ -7,30 +8,32 @@ terraform {
   }
 }
 
-# Asagida iki farkli subscription icin iki farkli provider tanimlandi.
-# Bu provider'larin her birine bir alias verildi. 
-# Alias kullanilarak her bir resource için hangi provider'in kullanilacagi belirtilebilir.
-
+# Bu blok, ilk abonelik için Azure sağlayıcısını tanımlar.
+# "sub1" takma adı, kodun diğer bölümlerinde bu sağlayıcıya atıfta bulunmak için kullanılır.
 provider "azurerm" {
-  subscription_id = ""
+  subscription_id = "" # İlk Azure aboneliği için abonelik ID'si
   alias           = "sub1"
-  features {}
+  features {} # Bu blok Azure sağlayıcısı tarafından gereklidir, ancak boş bırakılabilir
 }
 
+# Bu blok, ikinci abonelik için Azure sağlayıcısını tanımlar.
+# "sub2" takma adı, kodun diğer bölümlerinde bu sağlayıcıya atıfta bulunmak için kullanılır.
 provider "azurerm" {
-  subscription_id = ""
+  subscription_id = "" # İkinci Azure aboneliği için abonelik ID'si
   alias           = "sub2"
-  features {}
+  features {} # Bu blok Azure sağlayıcısı tarafından gereklidir, ancak boş bırakılabilir
 }
 
+# Bu blok, ilk Azure aboneliğinde bir kaynak grubu oluşturur
 resource "azurerm_resource_group" "rg" {
-  provider = azurerm.sub1
-  name     = "rg-sub1"
-  location = "westeurope"
+  provider = azurerm.sub1 # Bu kaynağın ilk abonelikte oluşturulması gerektiğini belirtir
+  name     = "rg-sub1"    # Kaynak grubunun adı
+  location = "westeurope" # Kaynak grubunun oluşturulacağı konum
 }
 
+# Bu blok, ikinci Azure aboneliğinde bir kaynak grubu oluşturur
 resource "azurerm_resource_group" "rg2" {
-  provider = azurerm.sub2
-  name     = "rg-sub2"
-  location = "westeurope"
+  provider = azurerm.sub2 # Bu kaynağın ikinci abonelikte oluşturulması gerektiğini belirtir
+  name     = "rg-sub2"    # Kaynak grubunun adı
+  location = "westeurope" # Kaynak grubunun oluşturulacağı konum
 }
