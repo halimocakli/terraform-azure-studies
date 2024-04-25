671187012385adc02a75e21886cbb0c3196ff5c4
halimocakli <halimcanocakli@gmail.com>
2024-04-25 13:48:43 +0300

    Add comments to main.tf

--- 02/03-Variables/main.tf
+++ 02/03-Variables/main.tf
@@ -1,3 +1,4 @@
+// Terraform yapılandırma bloğu, gereken sürüm ve sağlayıcıları belirtir
 terraform {
   required_version = "~> 1.0"
   required_providers {
@@ -8,27 +9,19 @@ terraform {
   }
 }
 
+// Azure Resource Manager sağlayıcısını belirtir
 provider "azurerm" {
   features {}
 }
 
+// Kaynak grubunun adını belirleyen değişkeni tanımlar
 variable "resource_group_name" {
   type        = string
   description = "The name of the resource group in which to create the resources."
 }
 
+// Kaynakların oluşturulacağı konumu belirleyen değişkeni tanımlar
 variable "location" {
   type        = string
   description = "The location/region where the resources will be created."
-  default     = "westeurope"
-
-  validation {
-    condition     = contains(["westeurope", "westus"], var.location)
-    error_message = "The location must be either westeurope or westus."
-  }
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
 }
