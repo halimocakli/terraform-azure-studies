be4353fa90b1ad58291da9e34e9d0501f1a99f71
halimocakli <halimcanocakli@gmail.com>
2024-04-30 15:00:08 +0300

    (feature):

--- 02/04-Sample-App/main.tf
+++ 02/04-Sample-App/main.tf
@@ -66,19 +66,3 @@ resource "azurerm_application_insights" "appinsight-app" {
     CreatedBy = var.createdby
   }
 }
-
-output "webapp_hostname" {
-  description = "Hostname of the webapp"
-  value       = azurerm_linux_web_app.app.default_hostname
-}
-
-output "webapp_name" {
-  description = "Name of the webapp"
-  value       = azurerm_linux_web_app.app.name
-}
-
-output "webapp_password" {
-  description = "Credential of the webapp"
-  value       = azurerm_linux_web_app.app.site_credential
-  sensitive   = true
-}
