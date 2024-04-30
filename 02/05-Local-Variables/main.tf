6992f71c099860e3c371814504352c28603f5075
halimocakli <halimcanocakli@gmail.com>
2024-04-30 13:42:37 +0300

    (feature): comments added

--- 02/05-Local-Variables/main.tf
+++ 02/05-Local-Variables/main.tf
@@ -1,36 +1,28 @@
-terraform {
-  required_version = "~> 1.0"
-  required_providers {
-    azurerm = {
-      version = "~> 3.18"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
+# Define the application name variable
 variable "application_name" {
   description = "The name of the application"
   type        = string
 }
 
+# Define the environment name variable
 variable "environment_name" {
   description = "The name of environment"
   type        = string
 }
 
+# Define the country code variable
 variable "country_code" {
   description = "The country code (FR-US-TR-...)"
   type        = string
 }
 
+# Create a resource group in Azure
 resource "azurerm_resource_group" "rg" {
   name     = "RG-${local.resource_name}"
   location = "westeurope"
 }
 
+# Create a public IP in Azure
 resource "azurerm_public_ip" "pip" {
   name                = "IP-${local.resource_name}"
   location            = "westeurope"
@@ -39,6 +31,8 @@ resource "azurerm_public_ip" "pip" {
   domain_name_label   = "halimsdomain"
 }
 
+# Define a local value for the resource name
+# Local variables reachs variables and resources only in the same file
 locals {
   resource_name = "${var.application_name}-${var.environment_name}-${var.country_code}"
 }
