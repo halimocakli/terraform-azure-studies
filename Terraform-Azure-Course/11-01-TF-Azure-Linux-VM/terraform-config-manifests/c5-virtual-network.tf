f996382a4d7c587fd59517c0c44d56377355ac12
halimocakli <halimcanocakli@gmail.com>
2024-07-19 15:41:02 +0300

    FEAT: Virtual Machine provisioning method on Azure by using Terraform

--- Terraform-Azure-Course/11-01-TF-Azure-Linux-VM/terraform-config-manifests/c5-virtual-network.tf
+++ Terraform-Azure-Course/11-01-TF-Azure-Linux-VM/terraform-config-manifests/c5-virtual-network.tf
@@ -20,7 +20,7 @@ resource "azurerm_public_ip" "myPublicIp" {
   resource_group_name = azurerm_resource_group.myRg.name
   location            = azurerm_resource_group.myRg.location
   allocation_method   = "Static"
-  domain_name_label   = "halims-app-vm-${random_string.myRandom.id}"
+  domain_name_label   = "halims-app-vm-${random_string.myRandomString.id}"
   tags = {
     environment = "Dev"
   }
